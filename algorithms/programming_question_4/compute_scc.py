573ba90eb03cc16a5957d9a3153c713973800ff1
vansickle <vansickle@vsmacbookair>
2012-04-16 12:15:38 +0400

    some works on algo pq#4

--- algorithms/programming_question_4/compute_scc.py
+++ algorithms/programming_question_4/compute_scc.py
@@ -5,15 +5,11 @@ def log(*args):
 	print " ".join([str(a) for a in args])
 	pass
 
+vert_number = 0
+#can change to list
 vertices = dict()
-	
-#need to have ordered list of vertices
-#cause dict.items() not ordered
-vertices_list = list()
 
 vertices_dict_by_finish_times = dict()
-vertices_list_by_finish_times = list()
-
 finish_time = 0
 
 def drill_down(vertex):
@@ -39,23 +35,28 @@ def drill_down(vertex):
 	global finish_time
 	finish_time += 1
 	vertex.finish_time = finish_time
-	vertices_list_by_finish_times.insert(0, vertex)
 	vertices_dict_by_finish_times[vertex.finish_time] = vertex
 	log("vertex ", vertex, "finish time:", finish_time)
 
 def compute_scc(list_):
 	
 	logw("starts computing")
-	list_ = reversed(list_)
+	reversed_list = reversed(list_)
 	logw("reversed")
-	for edge in list_:
+	for edge in reversed_list:
 		if edge.tail not in vertices:
 			vertex = Vertex(edge.tail)
 			vertices[edge.tail] = vertex
-			vertices_list.append(vertex)
 		
 		vertex = vertices[edge.tail]
 		vertex.add_edge(edge)
+		
+		if edge.head not in vertices:
+			head_vertex = Vertex(edge.head)
+			vertices[edge.head] = head_vertex
+		
+		head_vertex = vertices[edge.head]
+		head_vertex.add_inc_edge(edge)
 		
 		# log("edge: ", edge)
 		#for adjvi in adjlist[1:]:
@@ -64,13 +65,21 @@ def compute_scc(list_):
 	log("vertices:", vertices)
 	log("vertices list:", vertices)
 	
-	for vertex in vertices_list:
+	for i in xrange(vert_number,0,-1):
+		vertex = vertices[i]
 		log()
 		log("top level vertex", vertex)
 		if not vertex.is_explored():
 			drill_down(vertex)
-		
-		
+	
+	# log("\nreverse list")	
+	# list_ = map(lambda edge: edge.reverse(), list_)
+	# log(list_)
+	
+	for i in xrange(vert_number, 0, -1):
+		vertex = vertices[i]
+		vertex.reverse()
+	
 		
 
 class Vertex(object):
@@ -78,12 +87,16 @@ class Vertex(object):
 		super(Vertex, self).__init__()
 		self.number = number
 		self.edges = []
+		self.inc_edges = []
 		self.explored = False
 		self.finish_time = 0
 		
 	def add_edge(self, edge):
 		self.edges.append(edge)
-		
+	
+	def add_inc_edge(self, edge):
+		self.inc_edges.append(edge)
+	
 	def mark_as_explored(self):
 		self.explored = True
 	
@@ -92,6 +105,10 @@ class Vertex(object):
 		
 	def is_finished(self):
 		return self.finish_time>0
+	
+	def reverse(self):
+		map(lambda edge: edge.reverse(), self.edges)
+		self.edges, self.inc_edges = self.inc_edges, self.edges
 		
 	def __str__(self):
 		return "{0} : {1}".format(self.number, self.edges)
@@ -112,14 +129,20 @@ class Edge(object):
 	def __repr__(self):
 		return self.__str__();
 
+	def reverse(self):
+		self.tail, self.head = self.head, self.tail
+		return self
+
 
 str_ = open('TestData/scc_graph_1.txt', 'r').read()
+vert_number = 7
 # str_ = open('SCC.txt', 'r').read()
+# vert_number = 875714
 list_ = str_.split('\n')
 
-list_ = [Edge(node_list[0],node_list[1]) for node_list in [line.split() for line in list_]]
+list_ = [Edge(int(node_list[0]),int(node_list[1])) for node_list in [line.split() for line in list_]]
 
 compute_scc(list_)
 
-log(vertices_list_by_finish_times)
+log(vertices)
 # log(list_)
